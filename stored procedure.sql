4efd59df6d1918df6956e0a46009fb3cd7500d1f
paui <163615470+httppaui@users.noreply.github.com>
2024-10-17 17:58:45 +0800

    Add files via upload

---
+++ stored procedure.sql
@@ -0,0 +1,18 @@
+-- STORED PROCEDURE
+use midtermdb;
+-- create sp that will return information of employee
+DELIMITER $$
+CREATE PROCEDURE spEmployeeSelect
+( 
+	IN EmpID varchar(5),
+    IN LastName varchar(50)
+)
+BEGIN
+	SELECT * 
+    FROM Employee e
+    WHERE e.EmpID = IFNULL(EmpID, e.EmpID)
+    AND e.LastName = IFNULL(LastName, e.lastname);
+END
+$$
+
+call spEmployeeSelect(null, null);
